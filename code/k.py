a97fca07c6f1afa26d8dd28c9f2c820898a02bfc
Demetris <demflou@gmail.com>
2019-04-02 00:21:20 +0300

    Parsing

--- code/k.py
+++ code/k.py
@@ -2,6 +2,6 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
 
-df = spark.read.csv("../train.csv",header=True,sep="\t");
+df = spark.read.csv("../train.csv",header=False,sep="\t");
 
 print(df.collect())
