821eceda3ce8010f7ba4c48a070ead8903818905
Demetris <demflou@gmail.com>
2019-05-11 21:07:21 +0300

    Parser with functions and convert the ratings to 0/1

---
+++ code/rate.py
@@ -0,0 +1,74 @@
+from pyspark.sql.functions import udf
+from pyspark.sql.functions import col, regexp_replace, split
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import StopWordsRemover
+import logging
+import sys
+
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
+
+def lower_clean_str(x):
+  punc='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+  lowercased_str = x.lower()
+  for ch in punc:
+    lowercased_str = lowercased_str.replace(ch, '')
+  return lowercased_str
+
+
+'''def remove_extra_ws(x):  #alternative method to cut extra whitespaces 1/3
+  return " ".join(x.split())'''
+
+def rate_transform(x):
+    if ((x=="1") or (x=="2") or (x=="3")):
+        x="0"
+    elif ((x=="4") or (x=="5")):
+        x="1"
+    return x
+
+def parse_data(path):
+    spark = SparkSession.builder.appName("Parsing and removing stopwords").getOrCreate()
+    if (path == "../train.csv"):
+        lcs = udf(lower_clean_str)
+        # rews=udf(remove_extra_ws) #2/3
+        df = spark.read.csv(path, header=False, sep="\t");
+        df = df.withColumn("_c1", lcs("_c1"))
+        # df=df.withColumn("_c1",rews("_c1")) #3/3
+        expres = [split(col("_c1"), " ").alias("_c1")]
+        df = df.withColumn("_c1",*expres)
+        remover = StopWordsRemover(inputCol="_c1", outputCol="filtered")
+        swlist = remover.getStopWords()
+        swlist.append("")
+        remover.setStopWords(swlist)
+        remover.transform(df).select("filtered")
+        # fdf.show()
+        return df
+
+    elif (path == "../test.csv"):
+        lcs = udf(lower_clean_str)
+        # rews=udf(remove_extra_ws) #2/3
+        df = spark.read.csv(path, header=False, sep="\t");
+        df = df.withColumn("_c0", lcs("_c0"))
+        # df=df.withColumn("_c1",rews("_c1")) #3/3
+        expres = [split(col("_c0"), " ").alias("_c0")]
+        df = df.select(*expres)
+        remover = StopWordsRemover(inputCol="_c0", outputCol="filtered")
+        swlist = remover.getStopWords()
+        swlist.append("")
+        remover.setStopWords(swlist)
+        remover.transform(df).select("filtered").show(1, truncate=False)
+        return df
+
+    else:
+        print "Wrong File or Path"
+        return -1
+
+def main():
+    rt = udf(rate_transform)
+    path = sys.argv[1]
+    df = parse_data(path)
+    df = df.withColumn("_c0", rt("_c0"))
+    df.show()
+
+if __name__=="__main__":
+    main()
